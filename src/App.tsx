18ac13e4599f586262b70faf89578d1a15f58e58
kubasn <sosinkuba@gmail.com>
2022-09-27 15:38:03 +0200

    27.09 new commit v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,38 @@
-import * as esbuild from "esbuild-wasm";
 import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
-import { fetchPlugin } from "./plugins/fetch-plugin";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+import bundle from "./bundler";
+import CodeEditor from "./Components/CodeEditor";
+import Preview from "./Components/Preview";
 
 const App = () => {
-  const ref = useRef<any>();
-  const iframe = useRef<any>();
   const [input, setInput] = useState("");
-
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
-    });
-  };
-  useEffect(() => {
-    startService();
-  }, []);
+  const [code, setCode] = useState("");
 
   const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-    console.log(ref.current.transform);
-
-    //reload iframe to prevent errors
-    iframe.current.srcdoc = html;
-
-    const result = await ref.current.build({
-      entryPoints: ["index.js"],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        //! we define here string not variable. we want to override NODE_ENV
-        "process.env.NODE_ENV": '"production"',
-        global: "window",
-      },
-    });
-    // console.log(result);
-    // setCode(result.outputFiles[0].text);
+    const output = await bundle(input);
+    setCode(output);
     // try {
     //   eval(result.outputFiles[0].text);
     // } catch (err) {
     //   alert(err);
     // }
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
-
-  const html = `
-  <html>
-  <head></head>
-  <body>
-  <div id='root'></div>
-  <script>
-  window.addEventListener('message',(event)=>{
-    try{
-    eval(event.data);
-    } catch(err) {
-      const root = document.querySelector('#root');
-      root.innerHTML = '<div style="color:white; background-color:#f59c95">' + '<h4>Runtime error</h4>' + err + '</div>'
-    }
-  },false)
-  
-  </script>
-  </body>
-  </html>
-
-  
-  `;
 
   return (
     <div>
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <iframe
-        title="preview"
-        ref={iframe}
-        sandbox="allow-scripts"
-        srcDoc={html}
+      <CodeEditor
+        initialValue="const a = 1"
+        onChange={(value) => setInput(value)}
       />
+      <div>
+        <button
+          className="bg-slate-500 text-white p-2 rounded-sm my-2"
+          onClick={onClick}
+        >
+          Submit
+        </button>
+      </div>
+      <Preview code={code} />
     </div>
   );
 };
---
+++ src/Components/CodeEditor.tsx
@@ -0,0 +1,63 @@
+import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
+import React from "react";
+import prettier from "prettier";
+import parser from "prettier/parser-babel";
+
+import { useRef } from "react";
+
+interface CodeEditorProps {
+  initialValue: string;
+  onChange(value: string): void;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const editorRef = useRef<any>();
+
+  //first argument is function that when  called value is returned second->reference to editor itself
+  const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    editorRef.current = monacoEditor;
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue());
+    });
+  };
+
+  const onFormatClick = () => {
+    const unformattedCode = editorRef.current.getModel().getValue();
+    const formatted = prettier.format(unformattedCode, {
+      parser: "babel",
+      plugins: [parser],
+      semi: true,
+      singleQuote: true,
+    });
+    editorRef.current.setValue(formatted);
+  };
+
+  return (
+    <div className="group relative h-[100%] hover:">
+      <button
+        className="bg-sky-800 text-white rounded-sm absolute right-8 top-2 z-10 p-1 opacity-0 group-hover:opacity-100 transition-all duration-[1s]  "
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
+      <MonacoEditor
+        editorDidMount={onEditorDidMount}
+        language="javascript"
+        theme="dark"
+        value={initialValue}
+        height="300px"
+        options={{
+          wordWrap: "on",
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+      />
+    </div>
+  );
+};
+
+export default CodeEditor;

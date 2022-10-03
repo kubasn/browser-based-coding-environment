218d0ae2c01f1f34ae8007815591683f27f52fa6
kubasn <sosinkuba@gmail.com>
2022-10-03 21:18:14 +0200

    03.10 update 3

--- src/Components/CodeCell.tsx
+++ src/Components/CodeCell.tsx
@@ -15,6 +15,24 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ item }) => {
   const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector(({ bundles }) => bundles[item.id]);
+  //current cell code + all previous code
+  const cumulativeCode = useTypedSelector((state) => {
+    const { data, order } = state.cells;
+    const orderedCells = order.map((id) => data[id]);
+    const allCodes = [];
+    for (let i of orderedCells) {
+      if (i.type === "code") {
+        allCodes.push(i.content);
+      }
+      //if we get to current cell -> break function
+      if (i.id === item.id) {
+        break;
+      }
+    }
+    return allCodes;
+  });
+
+  console.log(cumulativeCode);
 
   useEffect(() => {
     if (!bundle) {
